4a4bea9e113285962129c093e7ac92542696fb68
dominictoms <dominictoms97@gmail.com>
2021-07-13 02:45:07 +0100

    update colours

--- config.h
+++ config.h
@@ -5,16 +5,16 @@
 
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Droid Sans Mono:size=9" };
+static const char dmenufont[]       = "Droid Sans Mono:size=9";
+static const char col_gray1[]       = "#373737";
+static const char col_gray2[]       = "#000000";
+static const char col_gray3[]       = "#c8c8c8";
+static const char col_gray4[]       = "#ffffff";
+static const char col_cyan[]        = "#000000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -32,6 +32,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "UnityHub", NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
